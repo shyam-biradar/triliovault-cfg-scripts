d84535289925f96f4dfee2281154b2ee4c3f83ba
Shyam Biradar <shyam.biradar@trilio.io>
2021-03-17 14:54:40 +0530

    Added tripleo container build scripts

--- redhat-director-scripts/tripleo-train/scripts/prepare_trilio_images_dockerhub.sh
+++ redhat-director-scripts/tripleo-train/scripts/prepare_trilio_images_dockerhub.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
+set -e
+
 if [ $# -lt 2 ];then
    echo "Script takes exacyly 2 argument"
-   echo -e "./prepare_trilio_images_podmanhub.sh <undercloud_hostname> <container_tag(queens)>"
+   echo -e "./prepare_trilio_images.sh <UNDERCLOUD_REGISTRY_HOSTNAME> <CONTAINER_TAG>"
    exit 1
 fi
 
@@ -11,32 +13,41 @@ tag=$2
 
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
+current_dir=$(pwd)
+base_dir="$(dirname $0)"
+
+if [ $base_dir = '.' ]
+then
+base_dir="$current_dir"
+fi
+
 source /home/stack/stackrc
 
+trilio_registry="docker.io"
+
+trilio_datamover_image_name="trilio-datamover-tripleo"
+trilio_datamover_api_image_name="trilio-datamover-api-tripleo"
+trilio_horizon_plugin_image_name="trilio-horizon-plugin-tripleo"
+
+
 ##Login to redhat container registry
-echo -e "Enter dockerhub account credentials"
-podman login docker.io
+echo -e "Enter trilio dockerhub credentials (Check with Trilio Team)"
+podman login ${trilio_registry}
+podman pull ${trilio_registry}/trilio/${trilio_horizon_plugin_image_name}:${tag}
+podman pull ${trilio_registry}/trilio/${trilio_datamover_image_name}:${tag}
+podman pull ${trilio_registry}/trilio/${trilio_datamover_api_image_name}:${tag}
 
-## Prepare openstack horizon with trilio container
-podman pull docker.io/trilio/trilio-horizon-plugin:${tag}
-podman tag docker.io/trilio/trilio-horizon-plugin:${tag} ${undercloud_hostname}:8787/trilio/trilio-horizon-plugin:${tag}
-openstack tripleo container image push --local ${undercloud_hostname}:8787/trilio/trilio-horizon-plugin:${tag}
+openstack tripleo container image push --local ${trilio_registry}/trilio/${trilio_datamover_image_name}:${tag}
 
-## Prepare trilio datamover container
-podman pull docker.io/trilio/trilio-datamover:${tag}
-podman tag docker.io/trilio/trilio-datamover:${tag} ${undercloud_hostname}:8787/trilio/trilio-datamover:${tag}
-openstack tripleo container image push --local ${undercloud_hostname}:8787/trilio/trilio-datamover:${tag}
+openstack tripleo container image push --local ${trilio_registry}/trilio/${trilio_datamover_api_image_name}:${tag}
 
-## Prepare trilio datamover api container
-podman pull docker.io/trilio/trilio-datamover-api:${tag}
-podman tag docker.io/trilio/trilio-datamover-api:${tag} ${undercloud_hostname}:8787/trilio/trilio-datamover-api:${tag}
-openstack tripleo container image push --local ${undercloud_hostname}:8787/trilio/trilio-datamover-api:${tag}
+openstack tripleo container image push --local ${trilio_registry}/trilio/${trilio_horizon_plugin_image_name}:${tag}
 
 
 ## Update image locations in env file
-trilio_dm_image="${undercloud_hostname}:8787\/trilio\/trilio-datamover:${tag}"
-trilio_dmapi_image="${undercloud_hostname}:8787\/trilio\/trilio-datamover-api:${tag}"
-trilio_horizon_image="${undercloud_hostname}:8787\/trilio\/trilio-horizon-plugin:${tag}"
+trilio_dm_image="${undercloud_hostname}:8787\/trilio\/trilio-datamover-tripleo:${tag}"
+trilio_dmapi_image="${undercloud_hostname}:8787\/trilio\/trilio-datamover-api-tripleo:${tag}"
+trilio_horizon_image="${undercloud_hostname}:8787\/trilio\/trilio-horizon-plugin-tripleo:${tag}"
 
 sed  -i "s/.*DockerTrilioDatamoverImage.*/\   DockerTrilioDatamoverImage:\ ${trilio_dm_image}/g" $SCRIPT_DIR/../environments/trilio_env.yaml
 sed  -i "s/.*DockerTrilioDmApiImage.*/   DockerTrilioDmApiImage: ${trilio_dmapi_image}/g" $SCRIPT_DIR/../environments/trilio_env.yaml
